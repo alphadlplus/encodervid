d24c6eed32630117cd2f2b564f14b885e1dabac7
alphadlplus <82231865+alphadlplus@users.noreply.github.com>
2022-08-07 01:26:42 +0430

    Update chat.py

--- chat.py
+++ chat.py
@@ -27,7 +27,7 @@ To give custom name to file send it with url seperated with |
 
 Time taken : {} seconds."""
     FILE_SIZE_ERROR = "ERROR : Cannot Extract File Size from URL!"
-    MAX_FILE_SIZE = "File size is greater than 2Gb. Which is the limit imposed by telegram!"
+    MAX_FILE_SIZE = "File size is greater than 4Gb. Which is the limit imposed by telegram!"
     LONG_CUS_FILENAME = """Filename you provided is greater than 60 characters.
 Please provide a shorter name."""
     UNSUPPORTED_FORMAT = "ERROR : File format {} Not supported!"
