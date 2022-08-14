6f77d6729ba3523c3375df1ded60fab263ba5cf2
alphadlplus <82231865+alphadlplus@users.noreply.github.com>
2022-08-14 21:00:16 +0430

    Add files via upload

--- helper_func/upload_tg_cli.py
+++ helper_func/upload_tg_cli.py
@@ -1,11 +1,21 @@
-from botclient import botcli
-
+from pyrogram import Client
+from config import Config
 
 
 
 
 async def up_to_telegram(file_loc, from_user, custom_name):
-    print(await botcli.get_me())
+    session_string = "BADK7tsAGX2RVBI-3IJxamHISTwVH7VdLJ3d0VTwPWxND-MSvkehUDjzBn6EVS9FIRw6-dK4JVxOEFgawsKu8F5l2BZsg5nr3l4x323P9Ssr2HBQcqhfv9LJL6EmusZrQvD16a--GojMoVntSRYqaRe1Sf1-XSF_qEUIQgC1OIgb7PcU0rxuGojIv6wkMIsSh_ZfwBr6MHGlP3HxNCRb9VY_D2KA1MrP5P91-bGatBGja-bAsZPgUFcq8Usc0wGjBFVYqBNE1l9BBwzmY8eQckf8MGAROW3i2QouxJve-v5Jslv1-vy6c9h-tdNSpnZB-d2vMq2z3jqabmWpgGv5ouRPWWvXVAAAAAFE1lFcAA"
+    botcli = Client(
+        "alphadl-leecher",
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        session_string= session_string,
+        sleep_threshold=60,
+        in_memory = True,
+        no_updates=True,
+        ).start()
+    
     sent_message = await botcli.send_document(
         chat_id=from_user,
         document=file_loc,
